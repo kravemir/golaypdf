97921ffe6f6322294ac581880bd26b4db9590733
Miroslav Kravec <kravec.miroslav@gmail.com>
2019-11-18 10:26:04 +0100

    Compute default line height from font size, and align content horizontally correctly (without a tiny space), and align content vertically to baseline

--- element/text.go
+++ element/text.go
@@ -1,25 +1,56 @@
 package element
 
-import . "github.com/kravemir/golaypdf"
+import (
+	. "github.com/kravemir/golaypdf"
+	"math"
+)
+
+const float64EqualityThreshold = 1e-9
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
 
 type Text struct {
 	Text string
 
-	LineHt float64
-	Align  string
+	LineHt          float64
+	HorizontalAlign string
 }
 
 func (c Text) Measure(context Context, width float64) (height float64, render Renderer) {
+	lineHt := c.LineHt
+
+	if almostEqual(lineHt, 0.0) {
+		_, unitSize := context.PDF().GetFontSize()
+		lineHt = unitSize * 1.4
+	}
+
+	descriptor := context.PDF().GetFontDesc("", "")
+	baseline := float64(descriptor.Ascent) / float64(descriptor.Ascent-descriptor.Descent)
+
 	textList := context.PDF().SplitText(c.Text, width)
-	height = float64(len(textList)) * c.LineHt
+	height = float64(len(textList)) * lineHt
 
 	return height, FuncToRenderer(func(context Context, x, y, w, h float64) {
-		cellY := y + (h-height)/2
+		lineY := y + lineHt*baseline
 
 		for _, textLine := range textList {
-			context.PDF().SetXY(x, cellY)
-			context.PDF().CellFormat(w, c.LineHt, textLine, "", 0, c.Align, false, 0, "")
-			cellY += c.LineHt
+			var lineX float64
+
+			switch c.HorizontalAlign {
+			default:
+				fallthrough
+			case "L":
+				lineX = x
+			case "C":
+				lineX = x + w/2 - context.PDF().GetStringWidth(textLine)/2
+			case "R":
+				lineX = x + w - context.PDF().GetStringWidth(textLine)
+			}
+
+			context.PDF().Text(lineX, lineY, textLine)
+			lineY += lineHt
 		}
 	})
 }
